7b16ee26e881559ab6a6c563ba9c083c60d5be0a
Vladislav Fedorov <73140488+ValDevelopment@users.noreply.github.com>
2025-08-01 17:02:22 -0500

    enemies attack, castle health

--- GMTK-2025/Assets/Scripts/Environment/Enemy.cs
+++ GMTK-2025/Assets/Scripts/Environment/Enemy.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
-    public int moveSpeed;
+    public float maxMoveSpeed;
+    float moveSpeed;
     public int attackDamage;
 
     public Transform target;
+
+    bool inRange;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = maxMoveSpeed;
     }
 
     // Update is called once per frame
@@ -22,19 +26,42 @@ public class Enemy : MonoBehaviour
     {
         transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), moveSpeed * Time.deltaTime);
 
+        /*
         if (Vector2.Distance(transform.position, target.position) <= 2f)
         {
             moveSpeed = 0;
-        }
+        }*/
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void DealDamage(HealthComponent health)
     {
-        if (collision.gameObject.CompareTag("Gate"))
+        health.onChangeHP(-5);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Gate"))
         {
-            
-                moveSpeed = 0;
-            
+            HealthComponent castleHealth = collision.GetComponent<HealthComponent>();
+            CastleManager castleManager = target.GetComponent<CastleManager>();
+            DealDamage(castleHealth);
+            transform.position = new Vector2(transform.position.x + castleManager.castlePushForce, transform.position.y);
+            moveSpeed = 0;
+        }
+
+        if(collision.CompareTag("Defender"))
+        {
+            HealthComponent defenderHealth = collision.GetComponent<HealthComponent>();
+            DealDamage(defenderHealth);
+            //transform.position = new Vector2(transform.position.x + castleManager.castlePushForce, transform.position.y);
+            moveSpeed = 0;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Gate"))
+        {
+            moveSpeed = maxMoveSpeed;
         }
     }
 }
