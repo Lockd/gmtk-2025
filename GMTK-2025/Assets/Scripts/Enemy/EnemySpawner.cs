d90f0a5e8af739348a3f2aba4c4357c3830016bd
Lockd <pikagrigoriy@gmail.com>
2025-08-03 18:09:04 +0200

    Tooltips adjustments

--- GMTK-2025/Assets/Scripts/Enemy/EnemySpawner.cs
+++ GMTK-2025/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,9 +1,6 @@
 
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
